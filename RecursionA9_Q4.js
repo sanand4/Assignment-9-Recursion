37b61a854c8f9d90214a875f6cddc6638f07a705
sanand4 <sanand8406@gmail.com>
2023-06-29 23:58:13 +0530

    first commit

---
+++ RecursionA9_Q4.js
@@ -0,0 +1,7 @@
+function calculateExponent(N, P) {
+  return Math.pow(N, P);
+}
+
+// Example usage:
+console.log(calculateExponent(5, 2)); // Output: 25
+console.log(calculateExponent(2, 5)); // Output: 32
